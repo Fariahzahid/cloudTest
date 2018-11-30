de2df938488a6a49d008765789d91485f3fc637f
Maximilian Stader <stader.maximilian@gmail.com>
2018-11-30 21:40:05 +0100

    changed api request templates and response codes added part of the code need for creating message in POST request

--- index.php
+++ index.php
@@ -7,12 +7,14 @@
 require_once __DIR__ . '/vendor/autoload.php';
 // require_once __DIR__ . '/db.php';
 
+$configuration = [
+    'settings' => [
+        'displayErrorDetails' => true,
+    ],
+];
+$c = new \Slim\Container($configuration);
+$app = new \Slim\App($c);
 
-$app = new Slim\App();
-
-// global $dbConn;
-// $dbConn = pg_connect("host=ec2-54-246-85-234.eu-west-1.compute.amazonaws.com dbname=dcr3qut0dr1rit user=huxpssrspgwwum password=5282cc466a257a47f323a72891b12b3fce7dd9478a25a028364f422652bb380e")
-// or die('Connection failed: ' . pg_last_error());
 
 
 /**
@@ -72,11 +74,43 @@ $app->GET('/messages/{userId}', function($request, $response, $args) {
  * Output-Formats: [application/json]
  */
 $app->POST('/messages', function($request, $response, $args) {
+            $parsedbody = $request->getParsedBody();
+            $end = false;
+            $id = null;
+            $senderId = null;
+            $text = null;
+            $read = null;
+            if(array_key_exists("id",$parsedbody) & !$end) {
+                $id =       $parsedbody["id"];
+                $response->write('id must not be in message!');
+                $response = $response->withStatus(422);
+                return $response;
+            }
             
+            if(array_key_exists("senderId",$parsedbody)) {
+                $senderId = $parsedbody["senderId"];
+            } else {
+                $response->write('senderId is missing in message!');
+                $response = $response->withStatus(422);
+                return $response;
+            }
+            if(array_key_exists("text",$parsedbody)) {
+                $text = $parsedbody["text"];
+            } else {
+                $response->write('text is missing in message!');
+                $response = $response->withStatus(422);
+                return $response;
+            }
+            if(array_key_exists("read",$parsedbody)) {
+                $read = $parsedbody["read"];
+            } else {
+                $response->write('read is missing in message!');
+                $response = $response->withStatus(422);
+                return $response;
+            }
             
-            
-            $body = $request->getParsedBody();
-            $response->write('How about implementing sendMessage as a POST method ?');
+            $response = $response->withStatus(200);
+//             $response->write('How about implementing sendMessage as a POST method ?');
             return $response;
             });
 
