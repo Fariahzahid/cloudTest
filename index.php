7c6a59ea78db7cd7905462b80c64f957b3a79dee
Maximilian Stader <stader.maximilian@gmail.com>
2018-12-01 22:43:00 +0100

    added new PutMessageModel finished put method for changing existing message minor changes for returning 500 when database errors occur postive responses when something succeeds

--- index.php
+++ index.php
@@ -73,6 +73,7 @@ $app->GET('/messages/{userId}', function($request, $response, $args) {
             $dbConn = pg_pconnect("host=ec2-54-246-85-234.eu-west-1.compute.amazonaws.com dbname=dcr3qut0dr1rit user=huxpssrspgwwum password=5282cc466a257a47f323a72891b12b3fce7dd9478a25a028364f422652bb380e");
             if (!$dbConn) {
                 echo "An error occured while connecting to database!\n";
+                return $response->withStatus(500);
             }
             $userId = $args["userId"];
             $messages_read = array();
@@ -82,6 +83,7 @@ $app->GET('/messages/{userId}', function($request, $response, $args) {
                     "SELECT * FROM read_confirmations WHERE receiver_id=" . $userId . " AND message_read=false;");
                 if (!$result) {
                     echo "An error occured while querying database.\n";
+                    return $response->withStatus(500);
                 }
                 while ($row = pg_fetch_row($result)) {
                     array_push($messages_read, $row[0]);
@@ -94,12 +96,14 @@ $app->GET('/messages/{userId}', function($request, $response, $args) {
                     "UPDATE read_confirmations SET message_read=true WHERE id IN ( $int_mess_read );");
                 if (!$result) {
                     echo "An error occured while querying database.\n";
+                    return $response->withStatus(500);
                 }
                 
                 $result = pg_query($dbConn,
                     "SELECT senderid, text FROM messages WHERE id IN ( $int_mess_read );");
                 if (!$result) {
                     echo "An error occured while querying database.\n";
+                    return $response->withStatus(500);
                 }
     
                 while ($row = pg_fetch_row($result)) {
@@ -127,8 +131,7 @@ $app->POST('/messages', function($request, $response, $args) {
             $senderId = null;
             $text = null;
             $receivers = null;
-            if(array_key_exists("id",$parsedbody) & !$end) {
-                $id =       $parsedbody["id"];
+            if(array_key_exists("id",$parsedbody)) {
                 $response->write('id must not be in message!');
                 $response = $response->withStatus(422);
                 return $response;
@@ -159,6 +162,7 @@ $app->POST('/messages', function($request, $response, $args) {
             $dbConn = pg_pconnect("host=ec2-54-246-85-234.eu-west-1.compute.amazonaws.com dbname=dcr3qut0dr1rit user=huxpssrspgwwum password=5282cc466a257a47f323a72891b12b3fce7dd9478a25a028364f422652bb380e");
             if (!$dbConn) {
                 echo "'There was an error while connecting to the database...";
+                return $response->withStatus(500);
             }
             
             $result = pg_query($dbConn, 'SELECT max(id) from messages');
@@ -169,17 +173,20 @@ $app->POST('/messages', function($request, $response, $args) {
                                         	id, "senderid", text, read)
                                     	VALUES (' . $highestId . ', ' . $senderId . ', \'' . $text . '\', ' . $highestId . ');');
             if (!$result) {
-                //echo "Ein Fehler ist aufgetreten.\n";
+                echo "An error occured while querying the database.\n";
+                return $response->withStatus(500);
             }
             
             foreach ($receivers AS $receiverId) {
                 $result = pg_query($dbConn, 'INSERT INTO public.read_confirmations(
                                             	id, message_read, receiver_id)
                                         	VALUES (' . $highestId . ', false,' . $receiverId . ');');
+                if (!$result) {
+                    echo "An error occured while querying the database.\n";
+                    return $response->withStatus(500);
+                }
             }
-            
-            $response = $response->withStatus(200);
-            return $response;
+            return $response->withStatus(200)->write("Message sent");
             });
 
 
@@ -190,11 +197,63 @@ $app->POST('/messages', function($request, $response, $args) {
  * Output-Formats: [application/json]
  */
 $app->PUT('/messages', function($request, $response, $args) {
+            $parsedbody = $request->getParsedBody();
+            $id = null;
+            $senderId = null;
+            $text = null;
+            $receivers = null;
+            if(array_key_exists("id",$parsedbody)) {
+                $id = $parsedbody["id"];
+            } else {
+                $response->write('id is missing in message!');
+                $response = $response->withStatus(422);
+                return $response;
+            }
             
+            if(array_key_exists("senderId",$parsedbody)) {
+                $senderId = $parsedbody["senderId"];
+            } else {
+                $response->write('senderId is missing in message!');
+                $response = $response->withStatus(422);
+                return $response;
+            }
+            if(array_key_exists("text",$parsedbody)) {
+                $text = $parsedbody["text"];
+            } else {
+                $response->write('text is missing in message!');
+                $response = $response->withStatus(422);
+                return $response;
+            }
+            if(array_key_exists("receivers",$parsedbody)) {
+                $response->write('receivers must not be in message!');
+                $response = $response->withStatus(422);
+                return $response;
+            }
             
+            $dbConn = pg_pconnect("host=ec2-54-246-85-234.eu-west-1.compute.amazonaws.com dbname=dcr3qut0dr1rit user=huxpssrspgwwum password=5282cc466a257a47f323a72891b12b3fce7dd9478a25a028364f422652bb380e");
+            if (!$dbConn) {
+                echo "'There was an error while connecting to the database...";
+                return $response->withStatus(500);
+            }
             
-            $body = $request->getParsedBody();
-            $response->write('How about implementing updateMessage as a PUT method ?');
+            $result = pg_query($dbConn, "SELECT COUNT(id) FROM messages WHERE id=$id;");
+            if (!$result) {
+                echo "An error occured while querying the database.\n";
+            } else {
+                $row = pg_fetch_row($result);
+                $messageFound = $row[0];
+                if($messageFound == 0) {
+                    return $response->withStatus(404)->write("Message not found!");
+                }
+            }
+            
+            $result = pg_query($dbConn, "UPDATE messages SET senderid=$senderId, text='$text' WHERE id=$id;");
+            if (!$result) {
+                echo "An error occured while querying the database.\n";
+                return $response->withStatus(500);
+            } else {
+                $response = $response->write('Message succesfully UPDATED');
+            }
             return $response;
             });
 
